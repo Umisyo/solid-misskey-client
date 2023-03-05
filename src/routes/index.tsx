f6231b5d07d8518bcf7ad2d06429320bee29da4a
Umisyo <s_kusunoki@yumemi.co.jp>
2023-03-05 22:53:43 +0900

    fix: 正常にリダイレクトするよう修正

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
+import { createEffect } from 'solid-js'
 import { Title, useNavigate } from 'solid-start'
 import TimeLine from '~/components/features/TimeLine'
 
 export default function Home() {
-  if (typeof localStorage === 'undefined' || !localStorage?.isLogin) {
-    const navigate = useNavigate()
-    navigate('/login')
-  }
+  createEffect(() => {
+    if (localStorage?.length === 0 || !localStorage?.getItem('isLogin')) {
+      const navigate = useNavigate()
+      navigate('/login')
+    }
+  })
   return (
     <main class="h-screen">
       <Title>Solid misskey client</Title>
