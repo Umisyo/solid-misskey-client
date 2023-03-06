d524b5f13ccc20c8280cb514aab1b133946ef160
Umisyo <s_kusunoki@yumemi.co.jp>
2023-03-07 02:27:30 +0900

    fix: 不要な記述を削除

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,14 +1,7 @@
-import { createEffect } from 'solid-js'
-import { Title, useNavigate } from 'solid-start'
+import { Title } from 'solid-start'
 import Login from '~/components/features/Login'
 
 export default function LoginPage() {
-  createEffect(() => {
-    if (localStorage?.getItem('isLogin')) {
-      const navigate = useNavigate()
-      navigate('/')
-    }
-  })
   return (
     <>
       <Title>login</Title>
